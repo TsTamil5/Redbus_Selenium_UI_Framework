e81433bfc318a32d3a3e86aa0f8a3b4ed789c2d7
Tamil <tstamilvs@gmail.com>
2026-04-18 21:51:09 +0530

    Pages and AllFunctionUtility Added

--- src/main/java/com/redbus/testing/utilities/Pages.java
+++ src/main/java/com/redbus/testing/utilities/Pages.java
@@ -1,5 +1,20 @@
 package com.redbus.testing.utilities;
 
-public class Pages {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
+import com.redbus.testing.pages.DashboardPage;
+import com.redbus.testing.pages.HotelSearchResultsPage;
+import com.redbus.testing.pages.HotelsPage;
+
+public class Pages {
+	public static DashboardPage dashboardPage;
+	public static HotelSearchResultsPage hotelSearchResultsPage ;
+	public static HotelsPage hotelsPage;
+	
+	public static void loadAllPages(WebDriver driver) {
+		dashboardPage=PageFactory.initElements(driver, DashboardPage.class);
+		hotelSearchResultsPage=PageFactory.initElements(driver, HotelSearchResultsPage.class);
+		hotelsPage=PageFactory.initElements(driver, HotelsPage.class);
+	}
 }
