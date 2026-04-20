68b693a0c94df9b9b1780bbc27eeeb0d6ce12c65
p-keerthi02 <keerthinagaraju2005@gmail.com>
2026-04-20 14:39:02 +0530

    Train features Added

--- src/test/java/com/redbus/testing/stepdefinition/Hooks.java
+++ src/test/java/com/redbus/testing/stepdefinition/Hooks.java
@@ -1,41 +1,9 @@
 package com.redbus.testing.stepdefinition;
 
-<<<<<<< HEAD
-import org.openqa.selenium.chrome.ChromeDriver;
-
-
-
-import com.redbus.testing.utilities.AllUtilityFunction;
-import com.redbus.testing.utilities.Base;
-import com.redbus.testing.utilities.Pages;
-
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-
-public class Hooks extends Base {
-	public static AllUtilityFunction util = new AllUtilityFunction();
-	 @Before
-	    public void setUp() {
-
-	        driver = new ChromeDriver();
-	        driver.manage().window().maximize();
-	        driver.get("https://www.redbus.in");
-
-	        // initialize all pages
-	        Pages.initPages(driver);
-	        
-	        util.initWorkbook();
-	    }
-
-	    @After
-	    public void tearDown() {
-//	        driver.quit();
-	        driver = null;  
-	    }
-=======
 import java.io.IOException;
->>>>>>> d4399ab3e8ca2447131fcae29e4ba59ab11ee582
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import com.redbus.testing.utilities.AllUtilityFunction;
@@ -48,34 +16,38 @@ import io.cucumber.java.Scenario;
 
 public class Hooks extends AllUtilityFunction {
 
-	@Before
-	public void openBrowser() throws IOException {
+    @Before
+    public void openBrowser() throws IOException {
 
-		// Launch Browser
-		Base.driver = new EdgeDriver();
+        Base.driver = new EdgeDriver();
 
-		// Read properties file
-		initPropertiesUtility("src/test/resources/Readers/CommonData.properties");
+        initPropertiesUtility("src/test/resources/Readers/CommonData.properties");
 
-		String URL = getPropertyData("url");
+        String URL = getPropertyData("url");
 
-		// Browser Settings
-		setMaximizeBrowser(Base.driver);
-		implicitlyWait(Base.driver, 5);
+        setMaximizeBrowser(Base.driver);
+        implicitlyWait(Base.driver, 5);
 
-		// Open Application
-		Base.driver.get(URL);
+        Base.driver.get(URL);
 
-		// Load Page Objects
-		Pages.loadAllPages(Base.driver);
-	}
+        Pages.loadAllPages(Base.driver);
+    }
 
-	@After
-	public void closeBrowser(Scenario scenario) {
+    @After
+    public void closeBrowser(Scenario scenario) {
 
-//		 Close Browser
-		if (Base.driver != null) {
-			Base.driver.quit();
-		}
-	}
+        // Screenshot on failure
+        if (scenario.isFailed()) {
+            byte[] screenshot = ((TakesScreenshot) Base.driver)
+                    .getScreenshotAs(OutputType.BYTES);
+
+            scenario.attach(screenshot, "image/png", "Failed Screenshot");
+        }
+
+        // Safe browser close
+        if (Base.driver != null) {
+            Base.driver.quit();
+            Base.driver = null;
+        }
+    }
 }
--- src/test/java/com/redbus/testing/testrunner/RunnerIO.java
+++ src/test/java/com/redbus/testing/testrunner/RunnerIO.java
@@ -1,24 +1,30 @@
 package com.redbus.testing.testrunner;
-
-import org.testng.annotations.DataProvider;
-
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(
-	features = "./src/test/java/com/redbus/testing/featurefiles/SearchHotels.feature",
-	glue = "com.redbus.testing.stepdefinition",
-//	plugin = {
-//			"pretty",
-//			"html:target/cucumber-report.html",
-//			"json:target/cucumber.json"
-//	},
-	dryRun = false
-)
-public class RunnerIO extends AbstractTestNGCucumberTests {
-//	@Override
-//	@DataProvider(parallel = true)
-//	public Object[][] scenarios() {
-//	    return super.scenarios();
-//	}
+	@CucumberOptions(
+	        features = "./src/test/java/com/redbus/testing/featurefiles/TrainTickets.feature",
+
+	        glue = {
+	                "com.redbus.testing.stepdefinition"
+	        },
+	//
+//	        plugin = {
+//	                "pretty",
+//	                "html:target/cucumber-report.html",
+//	                "json:target/cucumber.json"
+//	        },
+	//
+//	        tags = "@TrainSearch",
+
+	        dryRun = false
+//	        monochrome = true
+	)
+	public class RunnerIO extends AbstractTestNGCucumberTests {
+
+//	    @Override
+//	    @DataProvider(parallel = true)
+//	    public Object[][] scenarios() {
+//	        return super.scenarios();
+//	    }
 }
