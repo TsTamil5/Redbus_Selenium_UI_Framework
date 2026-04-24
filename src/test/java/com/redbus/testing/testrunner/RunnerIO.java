5c6e83b2d8b2fcc4e0fce7ff09734682740977cd
Nav14 <naveenkumarbaskar28@gmail.com>
2026-04-24 18:30:56 +0530

    Test Runner updated

--- src/test/java/com/redbus/testing/testrunner/RunnerIO.java
+++ src/test/java/com/redbus/testing/testrunner/RunnerIO.java
@@ -6,16 +6,11 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 import io.cucumber.testng.CucumberOptions;
 
-
-<<<<<<< HEAD
-@CucumberOptions(features = "F:\\Redbus_Selenium_UI_Framework\\src\\test\\java\\com\\redbus\\testing\\featurefiles\\BusTicket.feature", glue ="com.redbus.testing.stepdefinition", 
-=======
 @CucumberOptions(features =	{"./src/test/java/com/redbus/testing/featurefiles/BusTicket.feature",
 "./src/test/java/com/redbus/testing/featurefiles/TrainTickets.feature",
 "./src/test/java/com/redbus/testing/featurefiles/Accounts.feature"},
 
 		glue ="com.redbus.testing.stepdefinition", 	
->>>>>>> a4541c2b54b1e2ec161fb62aa44165f8a09709dc
 		plugin = {
 		"pretty", "html:target/cucumber-report.html",
 		"json:target/cucumber.json",
